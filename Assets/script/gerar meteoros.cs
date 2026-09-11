c853564832ee08f036101fd290dc02660bd322ed
SOPHIA ANDRADE DE SOUZA <sophia_a_souza@estudante.sesisenai.org.br>
2026-09-10 21:51:32 -0300

    m

---
+++ Assets/script/gerar meteoros.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] SpawnItens;
+    public float SpawnTime;
+    public float SpawnDelay;
+
+    [Header("Area de spawn (Linha Horiozontal")]
+    public float minX = -8f;
+    public float maxX = 8f;
+    public float spawnY = 6f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandom", SpawnTime, SpawnDelay);
+    }
+
+    void SpawnRandom()
+    {
+        int random = Random.Range(0, SpawnItens.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(minX, maxX), spawnY, 0f);
+        GameObject novoItem = Instantiate(SpawnItens[random], spawnPosition, Quaternion.identity);
+        Rigidbody2D rbItem = novoItem.GetComponent<Rigidbody2D>();
+        if (rbItem != null)
+        {
+            float randomXForce = Random.Range(-1f, 1f);
+            Vector2 direcaoQueda = new Vector2(randomXForce, -5f).normalized;
+
+            rbItem.linearVelocity = direcaoQueda * Random.Range(3f, 6f);
+        }
+    }
+}
--- Assets/script/obstaculo.cs
+++ Assets/script/obstaculo.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
-public class obstaculo : MonoBehaviour
+public class obstacle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+    private float rotationVelocity;
+
     void Start()
     {
         float tamanhometeoro = Random.Range(0.5f, 2f);
         transform.localScale = new Vector3(tamanhometeoro, tamanhometeoro, 1);
+        rotationVelocity = Random.Range(-50f, 50f);
+        rb = GetComponent<Rigidbody2D>();
+        Vector2 direcaoAleatoria = Random.insideUnitCircle;
+        rb.AddForce(direcaoAleatoria * 5f, ForceMode2D.Impulse);
+
+        float torqueAleatorio = Random.Range(-10f, 10f);
+        rb.AddTorque(torqueAleatorio);
     }
 
     void Update()
     {
-        
+        transform.Rotate(0, 0, rotationVelocity * Time.deltaTime);
     }
 }
